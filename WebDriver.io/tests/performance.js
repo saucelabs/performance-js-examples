6bf3e5ac586e65318096ed8ed6e545ba9bfa9662
farhan-sauce <farhan.chauhan@saucelabs.com>
2018-12-19 21:53:08 +0530

    [no-ticket] upgrade to webbdriver 5

--- WebDriver.io/tests/performance.js
+++ WebDriver.io/tests/performance.js
@@ -3,31 +3,33 @@ const assert = require('assert')
 describe('Performance Demo Test', () => {
     before(() => {
         browser.url('/');
-        browser.setValue('[data-test="username"]', 'standard_user')
-        browser.setValue('[data-test="password"]', 'secret_sauce')
-        browser.click('.login-button')
-        browser.waitForVisible('#inventory_container')
+        const username = $('[data-test="username"]')
+        const password = $('[data-test="username"]')
+        const loginButton = $('.login-button')
+        username.setValue('standard_user')
+        password.setValue('secret_sauce')
+        loginButton.click()
     })
 
     it('(sauce:network) should make a request for main.js', () => {
-        const { value: requests } = browser.log('sauce:network')
+        const { value: requests } = browser.getLogs('sauce:network')
         const isRequestExists = requests.some((req) => req.url.includes('main.js'))
         assert.strictEqual(isRequestExists, true);
     })
 
     it('(sauce:metrics) should check pageLoadTime', () => {
-        const { value: metrics } = browser.log('sauce:metrics');
+        const { value: metrics } = browser.getLogs('sauce:metrics');
         const pageLoadTime = metrics.domContentLoaded - metrics.navigationStart
         assert.ok(pageLoadTime <= 5, `Expected page load time to be lower than 5s but was ${pageLoadTime}s`)
     })
 
     it('(sauce:timing) should check timing', () => {
-        const { value: timing } = browser.log('sauce:timing');
+        const { value: timing } = browser.getLogs('sauce:timing');
         assert.ok('domLoading' in timing, `domLoading is missing`)
     })
 
     it('(sauce:performance) should check speedIndex', () => {
-        const { value: performance } = browser.log('sauce:performance');
+        const { value: performance } = browser.getLogs('sauce:performance');
         assert.ok('speedIndex' in performance, `SpeedIndex is missing`)
     })
 
