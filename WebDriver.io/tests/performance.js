df152df1f74da9d711ddba6804cf983c8a24ed2a
farhan-sauce <farhan.chauhan@saucelabs.com>
2018-12-20 14:34:13 +0530

    [no-ticket] update dependencies

--- WebDriver.io/tests/performance.js
+++ WebDriver.io/tests/performance.js
@@ -4,39 +4,39 @@ describe('Performance Demo Test', () => {
     before(() => {
         browser.url('/');
         const username = $('[data-test="username"]')
-        const password = $('[data-test="username"]')
-        const loginButton = $('.login-button')
         username.setValue('standard_user')
+        const password = $('[data-test="password"]')
         password.setValue('secret_sauce')
+        const loginButton = $('.login-button')
         loginButton.click()
     })
 
     it('(sauce:network) should make a request for main.js', () => {
-        const { value: requests } = browser.getLogs('sauce:network')
-        const isRequestExists = requests.some((req) => req.url.includes('main.js'))
+        const network = browser.getLogs('sauce:network')
+        const isRequestExists = network.some((req) => req.url.includes('main.js'))
         assert.strictEqual(isRequestExists, true);
     })
 
     it('(sauce:metrics) should check pageLoadTime', () => {
-        const { value: metrics } = browser.getLogs('sauce:metrics');
+        const metrics = browser.getLogs('sauce:metrics');
         const pageLoadTime = metrics.domContentLoaded - metrics.navigationStart
         assert.ok(pageLoadTime <= 5, `Expected page load time to be lower than 5s but was ${pageLoadTime}s`)
     })
 
     it('(sauce:timing) should check timing', () => {
-        const { value: timing } = browser.getLogs('sauce:timing');
+        const timing = browser.getLogs('sauce:timing');
         assert.ok('domLoading' in timing, `domLoading is missing`)
     })
 
     it('(sauce:performance) should check speedIndex', () => {
-        const { value: performance } = browser.getLogs('sauce:performance');
+        const performance = browser.getLogs('sauce:performance');
         assert.ok('speedIndex' in performance, `SpeedIndex is missing`)
     })
 
     it('(sauce:hello) should return test name', () => {
-        const { value: output } = browser.execute('sauce:hello', {
-            name: browser.desiredCapabilities.name
+        const output = browser.execute('sauce:hello', {
+            name: browser.config.capabilities.name
         });
-        assert.ok(output.includes(browser.desiredCapabilities.name), `Test name is missing`)
+        assert.ok(output.includes(browser.config.capabilities.name), `Test name is missing`)
     })
 })
