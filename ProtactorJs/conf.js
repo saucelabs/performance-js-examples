c1d91d3f00272b1c53223edcffafb24beef8aeb9
farhan-sauce <farhan.chauhan@saucelabs.com>
2018-12-28 15:51:54 +0530

    [PERF-298] fix eslint issue

--- ProtactorJs/conf.js
+++ ProtactorJs/conf.js
@@ -1,34 +1,34 @@
 exports.config = {
-    sauceUser: process.env.SAUCE_USERNAME,
-    sauceKey: process.env.SAUCE_ACCESS_KEY,
-    specs: ['specs/*spec.js'],
-    capabilities: {
-        platform: "OS X 10.10",
-        version: "70.0",
-        browserName: "chrome",
-        extendedDebugging: true,
-        crmuxdriverVersion: "stable",
-        name: "Performance Testing",
-        loggingPrefs: {
-            'driver': 'ALL',
-            'browser': 'ALL',
-            'performance': 'ALL',
-            'server': 'ALL'
-        },
-        chromeOptions: {
-            perfLoggingPrefs: {
-                enableNetwork: true,
-                enablePage: true
-            }
-        }
-    },
-    baseUrl: 'https://www.saucedemo.com',
-    onComplete: function () {
-        var printSessionId = function (jobName) {
-            browser.getSession().then(function (session) {
-                console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
-            });
-        }
-        printSessionId("Performance Testing");
-    }
-}
+	sauceUser: process.env.SAUCE_USERNAME,
+	sauceKey: process.env.SAUCE_ACCESS_KEY,
+	specs: ['specs/*spec.js'],
+	capabilities: {
+		platform: 'OS X 10.10',
+		version: '70.0',
+		browserName: 'chrome',
+		extendedDebugging: true,
+		crmuxdriverVersion: 'stable',
+		name: 'Performance Testing',
+		loggingPrefs: {
+			driver: 'ALL',
+			browser: 'ALL',
+			performance: 'ALL',
+			server: 'ALL',
+		},
+		chromeOptions: {
+			perfLoggingPrefs: {
+				enableNetwork: true,
+				enablePage: true,
+			},
+		},
+	},
+	baseUrl: 'https://www.saucedemo.com',
+	onComplete() {
+		const printSessionId = (jobName) => {
+			browser.getSession().then((session) => {
+				console.log(`SauceOnDemandSessionID=${session.getId()} job-name=${jobName}`);
+			});
+		};
+		printSessionId('Performance Testing');
+	},
+};
