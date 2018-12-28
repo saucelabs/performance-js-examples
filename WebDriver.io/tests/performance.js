02e780c0bac993c1ad0a04a280a010740b8cee93
farhan-sauce <farhan.chauhan@saucelabs.com>
2018-12-28 15:35:21 +0530

    [PERF-298] use eslint and fix indentation issue

--- WebDriver.io/tests/performance.js
+++ WebDriver.io/tests/performance.js
@@ -1,53 +1,53 @@
-const assert = require('assert')
+const assert = require('assert');
 
 describe('Performance Demo Test', () => {
-    before(() => {
-        browser.url('/');
-        const username = $('[data-test="username"]')
-        username.setValue('standard_user')
-        const password = $('[data-test="password"]')
-        password.setValue('secret_sauce')
-        const loginButton = $('.login-button')
-        loginButton.click()
-    })
+	before(() => {
+		browser.url('/');
+		const username = $('[data-test="username"]');
+		username.setValue('standard_user');
+		const password = $('[data-test="password"]');
+		password.setValue('secret_sauce');
+		const loginButton = $('.login-button');
+		loginButton.click();
+	});
 
-    it('(sauce:network) should make a request for main.js', () => {
-        const network = browser.getLogs('sauce:network')
-        const isRequestExists = network.some((req) => req.url.includes('main.js'))
-        assert.strictEqual(isRequestExists, true);
-    })
+	it('(sauce:network) should make a request for main.js', () => {
+		const network = browser.getLogs('sauce:network');
+		const isRequestExists = network.some(req => req.url.includes('main.js'));
+		assert.strictEqual(isRequestExists, true);
+	});
 
-    it('(sauce:metrics) should check pageLoadTime', () => {
-        const metrics = browser.getLogs('sauce:metrics');
-        const pageLoadTime = metrics.domContentLoaded - metrics.navigationStart
-        assert.ok(pageLoadTime <= 5, `Expected page load time to be lower than 5s but was ${pageLoadTime}s`)
-    })
+	it('(sauce:metrics) should check pageLoadTime', () => {
+		const metrics = browser.getLogs('sauce:metrics');
+		const pageLoadTime = metrics.domContentLoaded - metrics.navigationStart;
+		assert.ok(pageLoadTime <= 5, `Expected page load time to be lower than 5s but was ${pageLoadTime}s`);
+	});
 
-    it('(sauce:timing) should check timing', () => {
-        const timing = browser.getLogs('sauce:timing');
-        assert.ok('domLoading' in timing, `domLoading is missing`)
-    })
+	it('(sauce:timing) should check timing', () => {
+		const timing = browser.getLogs('sauce:timing');
+		assert.ok('domLoading' in timing, 'domLoading is missing');
+	});
 
-    it('(sauce:performance) should check speedIndex', () => {
-        const metrics = [
-            'load',
-            'speedIndex',
-            'pageWeight',
-            'pageWeightEncoded',
-            'timeToFirstByte',
-            'timeToFirstInteractive',
-            'firstContentfulPaint',
-            'perceptualSpeedIndex',
-            'domContentLoaded'
-        ];
-        const performance = browser.getLogs('sauce:performance');
-        metrics.forEach((metric) => assert.ok(metric in performance, `${metric} metric is missing`));
-    })
+	it('(sauce:performance) should check speedIndex', () => {
+		const metrics = [
+			'load',
+			'speedIndex',
+			'pageWeight',
+			'pageWeightEncoded',
+			'timeToFirstByte',
+			'timeToFirstInteractive',
+			'firstContentfulPaint',
+			'perceptualSpeedIndex',
+			'domContentLoaded',
+		];
+		const performance = browser.getLogs('sauce:performance');
+		metrics.forEach(metric => assert.ok(metric in performance, `${metric} metric is missing`));
+	});
 
-    it('(sauce:hello) should return test name', () => {
-        const output = browser.execute('sauce:hello', {
-            name: browser.config.capabilities.name
-        });
-        assert.ok(output.includes(browser.config.capabilities.name), `Test name is missing`)
-    })
-})
+	it('(sauce:hello) should return test name', () => {
+		const output = browser.execute('sauce:hello', {
+			name: browser.config.capabilities.name,
+		});
+		assert.ok(output.includes(browser.config.capabilities.name), 'Test name is missing');
+	});
+});
--- WebDriver.io/wdio.conf.js
+++ WebDriver.io/wdio.conf.js
@@ -1,33 +1,33 @@
 exports.config = {
-    user: process.env.SAUCE_USERNAME,
-    key: process.env.SAUCE_ACCESS_KEY,
-    specs: [
-	    './tests/**/*.js'
-    ],
-    maxInstances: 40,
-    capabilities: [{
-        browserName: 'chrome',
-        platform: 'OS X 10.10',
-        version: '70.0',
-        extendedDebugging: true,
-        crmuxdriverVersion: 'stable',
-        name: 'Performance testing',
-    }],
-    sync: true,
-    logLevel: 'error',
-    coloredLogs: true,
-    baseUrl: 'https://www.saucedemo.com',
-    waitforTimeout: 100000,
-    connectionRetryTimeout: 90000,
-    connectionRetryCount: 3,
-    services: ['sauce'],
-    framework: 'mocha',
-    reporters: ['dot'],
-    reporterOptions: {
-        outputDir: './'
-    },
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 100000
-    }
-}
+	user: process.env.SAUCE_USERNAME,
+	key: process.env.SAUCE_ACCESS_KEY,
+	specs: [
+		'./tests/**/*.js',
+	],
+	maxInstances: 40,
+	capabilities: [{
+		browserName: 'chrome',
+		platform: 'OS X 10.10',
+		version: '70.0',
+		extendedDebugging: true,
+		crmuxdriverVersion: 'stable',
+		name: 'Performance testing',
+	}],
+	sync: true,
+	logLevel: 'error',
+	coloredLogs: true,
+	baseUrl: 'https://www.saucedemo.com',
+	waitforTimeout: 100000,
+	connectionRetryTimeout: 90000,
+	connectionRetryCount: 3,
+	services: ['sauce'],
+	framework: 'mocha',
+	reporters: ['dot'],
+	reporterOptions: {
+		outputDir: './',
+	},
+	mochaOpts: {
+		ui: 'bdd',
+		timeout: 100000,
+	},
+};
