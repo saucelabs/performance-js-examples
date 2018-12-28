edf008fd95300c94c245d26eb86485efdb2e6ab6
farhan-sauce <farhan.chauhan@saucelabs.com>
2018-12-28 15:41:27 +0530

    [PERF-298] fix indent issue

--- NightWatch/nightwatch.conf.js
+++ NightWatch/nightwatch.conf.js
@@ -1,9 +1,10 @@
-module.exports = (function(settings) {
-    if (process.env.SELENIUM_HOST) {
-      settings.selenium.host = process.env.SELENIUM_HOST;
-    }
-    if (process.env.SELENIUM_PORT) {
-      settings.selenium.host = process.env.SELENIUM_PORT;
-    }
-    return settings;
+module.exports = ((defaultSetting) => {
+	const settings = defaultSetting;
+	if (process.env.SELENIUM_HOST) {
+		settings.selenium.host = process.env.SELENIUM_HOST;
+	}
+	if (process.env.SELENIUM_PORT) {
+		settings.selenium.host = process.env.SELENIUM_PORT;
+	}
+	return settings;
 })(require('./nightwatch.json'));
