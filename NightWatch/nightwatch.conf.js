ec710aac9dfbe27b2453d5304ece26e3ca6decf6
farhan-sauce <farhan.chauhan@saucelabs.com>
2019-01-02 15:01:04 +0530

    [No-ticket] update NightWatch version

--- NightWatch/nightwatch.conf.js
+++ NightWatch/nightwatch.conf.js
@@ -1,10 +1,10 @@
 module.exports = ((defaultSetting) => {
 	const settings = defaultSetting;
 	if (process.env.SELENIUM_HOST) {
-		settings.selenium.host = process.env.SELENIUM_HOST;
+		settings.selenium_server.selenium.host = process.env.SELENIUM_HOST;
 	}
 	if (process.env.SELENIUM_PORT) {
-		settings.selenium.host = process.env.SELENIUM_PORT;
+		settings.selenium_server.selenium.host = process.env.SELENIUM_PORT;
 	}
 	return settings;
 })(require('./nightwatch.json'));
