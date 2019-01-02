872476fd4f7852393a9772a94a67d12e52eee533
farhan-sauce <farhan.chauhan@saucelabs.com>
2019-01-02 14:46:51 +0530

    [PERF-298] refactor code

--- NightWatch/custom_commands/customSauceEnd.js
+++ NightWatch/custom_commands/customSauceEnd.js
@@ -1,24 +1,22 @@
-exports.command = function(callback) {
-    var SauceLabs = require("saucelabs");
+const SauceLabs = require('saucelabs');
 
-    var saucelabs = new SauceLabs({
-        username: process.env.SAUCE_USERNAME,
-        password: process.env.SAUCE_ACCESS_KEY
-    });
-
-    var sessionid = this.capabilities['webdriver.remote.sessionid'],
-        jobName = this.currentTest.name,
-        passed = this.currentTest.results.testcases[jobName].failed === 0;
-
-
-    console.log("SauceOnDemandSessionID=" + sessionid + " job-name=" + jobName);
-
-	var self = this
-	saucelabs.updateJob(sessionid, {
-    passed: passed,
-    name: jobName
-	}, function () {
-    	self.end(callback)
+exports.command = function customSauce(callback) {
+	const saucelabs = new SauceLabs({
+		username: process.env.SAUCE_USERNAME,
+		password: process.env.SAUCE_ACCESS_KEY,
 	});
 
+	const self = this;
+	const sessionid = this.capabilities['webdriver.remote.sessionid'];
+	const jobName = this.currentTest.name;
+	const passed = this.currentTest.results.testcases[jobName].failed === 0;
+
+	console.log(`SauceOnDemandSessionID=${sessionid} job-name=${jobName}`);
+
+	saucelabs.updateJob(sessionid, {
+		passed,
+		name: jobName,
+	}, () => {
+		self.end(callback);
+	});
 };
