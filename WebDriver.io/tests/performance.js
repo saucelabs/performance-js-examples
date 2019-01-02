101437bb05307d9e68634b97a11c4891dc480193
farhan-sauce <39295560+farhan-sauce@users.noreply.github.com>
2019-01-02 14:43:14 +0530

    [PERF-298] Sample test using: Webdriver.io framework (#2)
    * [PERF-298] add sample script using webdriverio
    
    * [no-ticket] upgrade to webdriver 5 (#1)
    
    * [no-ticket] upgrade to webbdriver 5
    
    * [no-ticket] update packages
    
    * [no-ticket] update dependencies
    
    * [PERF-298] remove unused dependencies
    
    * [PERF-298] minor changes in tests
    
    * [PERF-298] use eslint and fix indentation issue
    
    * [PERF-298] use inventory.html

---
+++ WebDriver.io/tests/performance.js
@@ -0,0 +1,47 @@
+const assert = require('assert');
+
+describe('Performance Demo Test', () => {
+	before(() => {
+		browser.url('/inventory.html');
+	});
+
+	it('(sauce:network) should make a request for main.js', () => {
+		const network = browser.getLogs('sauce:network');
+		const isRequestExists = network.some(req => req.url.includes('main.js'));
+		assert.strictEqual(isRequestExists, true);
+	});
+
+	it('(sauce:metrics) should check pageLoadTime', () => {
+		const metrics = browser.getLogs('sauce:metrics');
+		const pageLoadTime = metrics.domContentLoaded - metrics.navigationStart;
+		assert.ok(pageLoadTime <= 5, `Expected page load time to be lower than 5s but was ${pageLoadTime}s`);
+	});
+
+	it('(sauce:timing) should check timing', () => {
+		const timing = browser.getLogs('sauce:timing');
+		assert.ok('domLoading' in timing, 'domLoading is missing');
+	});
+
+	it('(sauce:performance) should check speedIndex', () => {
+		const metrics = [
+			'load',
+			'speedIndex',
+			'pageWeight',
+			'pageWeightEncoded',
+			'timeToFirstByte',
+			'timeToFirstInteractive',
+			'firstContentfulPaint',
+			'perceptualSpeedIndex',
+			'domContentLoaded',
+		];
+		const performance = browser.getLogs('sauce:performance');
+		metrics.forEach(metric => assert.ok(metric in performance, `${metric} metric is missing`));
+	});
+
+	it('(sauce:hello) should return test name', () => {
+		const output = browser.execute('sauce:hello', {
+			name: browser.config.capabilities.name,
+		});
+		assert.ok(output.includes(browser.config.capabilities.name), 'Test name is missing');
+	});
+});
