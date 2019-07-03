c4aa222ce1b208827cdfb65f75347340db0edd84
christian-bromann <mail@christian-bromann.com>
2019-07-03 13:11:23 +0200

    updated WebdriverIO examples

--- WebDriver.io/tests/performance.js
+++ WebDriver.io/tests/performance.js
@@ -1,61 +1,51 @@
-const assert = require('assert');
+const assert = require('assert')
 
 describe('Performance Testing', function () { // eslint-disable-line func-names
-	const { title } = this;
-	before(() => {
-		browser.url('/');
-		const username = $('[data-test="username"]');
-		username.setValue(process.env.PERF_USERNAME || 'standard_user');
-		const password = $('[data-test="password"]');
-		password.setValue('secret_sauce');
-		const loginButton = $('.btn_action');
-		loginButton.click();
-		browser.url('/inventory.html');
-	});
+    const { title } = this
 
-	it('logs (sauce:performance) should check speedIndex', () => {
-		const metrics = [
-			'load',
-			'speedIndex',
-			'pageWeight',
-			'pageWeightEncoded',
-			'timeToFirstByte',
-			'timeToFirstInteractive',
-			'firstContentfulPaint',
-			'perceptualSpeedIndex',
-			'domContentLoaded',
-		];
-		const performance = browser.getLogs('sauce:performance');
-		metrics.forEach(metric => assert.ok(metric in performance, `${metric} metric is missing`));
-	});
+    before(() => {
+        browser.url('https://www.saucedemo.com')
 
-	it('(sauce:performance) custom command should assert pageload has not regressed', () => {
-		const metric = 'load';
-		const output = browser.execute('sauce:performance', {
-			name: title,
-			metrics: [metric],
-		});
-		const { reason, result, details } = output;
-		/* The custom command will return 'pass' if the test falls within the predicted baseline
- 		 * or 'fail'  if the performance metric falls outside the predicted baseline.
-		 * customers can decide how strict they want to be in failing tests by setting thier own
-		 * failure points.
-		 * assert(details[metric].actual < 5000, true, reason);
-		 */
-		return result !== 'pass'
-			? assert.equal(details[metric].actual < 5000, true, reason)
-			: assert(result, 'pass');
-	});
+        const username = process.env.PERF_USERNAME || 'standard_user'
+        $('[data-test="username"]').setValue(username)
+        $('[data-test="password"]').setValue('secret_sauce')
+        $('.btn_action').click()
+    })
 
-	it('(sauce:performance) custom command should assert timeToFirstInteractive has not regressed', () => {
-		const metric = 'load';
-		const output = browser.execute('sauce:performance', {
-			name: title,
-			metrics: [metric],
-		});
-		const { reason, result, details } = output;
-		return result !== 'pass'
-			? assert.equal(details[metric].actual < 5000, true, reason)
-			: assert(result, 'pass');
-	});
-});
+    it('logs (sauce:performance) should check if all metrics were captured', () => {
+        const metrics = [
+            'estimatedInputLatency',
+            'timeToFirstByte',
+            'domContentLoaded',
+            'firstVisualChange',
+            'firstPaint',
+            'firstContentfulPaint',
+            'firstMeaningfulPaint',
+            'lastVisualChange',
+            'firstCPUIdle',
+            'firstInteractive',
+            'load',
+            'speedIndex',
+        ]
+        const performance = browser.execute('sauce:log', { type: 'sauce:performance' })
+        metrics.forEach(metric => assert.ok(metric in performance, `${metric} metric is missing`))
+    })
+
+    /* The custom command will return 'pass' if the test falls within the predicted baseline
+     * or 'fail'  if the performance metric falls outside the predicted baseline.
+     * customers can decide how strict they want to be in failing tests by setting thier own
+     * failure points.
+     * assert(details[metric].actual < 5000, true, reason)
+     */
+    it('(sauce:performance) custom command should assert pageload and speedIndex has not regressed', () => {
+        const output = browser.execute('sauce:performance', {
+            name: title,
+            metrics: ['speedIndex', 'load'],
+        })
+        const { result, details } = output
+        return assert.equal(
+            result, 'pass',
+            `Regression detected for metrics ${JSON.stringify(details, null, 4)}`,
+        )
+    })
+})
