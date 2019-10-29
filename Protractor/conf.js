0493a10b8cfb6051a6c93c7b6626317e22b781ef
Yeh Fang <yeh.fang@saucelabs.com>
2019-10-29 14:52:15 -0700

    update specs run

--- Protractor/conf.js
+++ Protractor/conf.js
@@ -17,7 +17,7 @@ const capabilities = {
 exports.config = {
     sauceUser: username,
     sauceKey: password,
-    specs: ['specs/performance.js'],
+    specs: ['specs/*.js'],
     capabilities,
     baseUrl: 'https://www.saucedemo.com',
     jasmineNodeOpts: {
