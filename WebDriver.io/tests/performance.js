ba30a56b8f85005c23f9b0bd5a68f83d455eb590
farhan-sauce <farhan.chauhan@saucelabs.com>
2020-12-04 18:15:33 +0530

    Performance budget test

--- WebDriver.io/tests/performance.js
+++ WebDriver.io/tests/performance.js
@@ -10,26 +10,55 @@ describe('Performance Testing', () => {
         $('.btn_action').click()
     })
 
-    it('logs (sauce:performance) should check if all metrics were captured', () => {
-        const metrics = [
-            'estimatedInputLatency',
-            'timeToFirstByte',
-            'domContentLoaded',
-            'firstVisualChange',
-            'firstPaint',
-            'firstContentfulPaint',
-            'firstMeaningfulPaint',
-            'lastVisualChange',
-            'firstCPUIdle',
-            'firstInteractive',
-            'load',
-            'speedIndex',
-            'largestContentfulPaint',
-            'totalBlockingTime',
-            'cumulativeLayoutShift'
-        ]
-        const performance = browser.execute('sauce:log', { type: 'sauce:performance' })
-        metrics.forEach(metric => assert.ok(metric in performance, `${metric} metric is missing`))
+    it('should be within performance budget', () => {
+        const performanceLog = browser.execute('sauce:log', { type: 'sauce:performance' })
+        /**
+         * Sample performanceLog
+         *  {
+                load: 754,
+                speedIndex: 652,
+                firstInteractive: 856,
+                firstVisualChange: 483,
+                lastVisualChange: 967,
+                firstMeaningfulPaint: 319,
+                firstCPUIdle: 856,
+                timeToFirstByte: 121,
+                firstPaint: 319,
+                estimatedInputLatency: 17,
+                firstContentfulPaint: 319,
+                totalBlockingTime: 76,
+                score: 0.99,
+                domContentLoaded: 238,
+                cumulativeLayoutShift: 0,
+                serverResponseTime: 121,
+                largestContentfulPaint: 960
+            }
+         */
+
+        const performanceBudget = {
+            'load': { 'min': 100, 'max': 1000 },
+            'firstContentfulPaint': { 'min': 100, 'max': 1000 },
+            'largestContentfulPaint': { 'min': 100 , 'max': 1000 },
+            'firstInteractive': { 'min': 100 , 'max': 1000 },
+            'speedIndex': { 'min': 100 , 'max': 1000 },
+            'score': { 'min': 0.9, 'max': 1 }
+        }
+
+        const failedMetrics = {}
+        Object.entries(performanceBudget).forEach(([ metric, budget ]) => {
+            const value = performanceLog[metric];
+            if (value < budget.min || value > budget.max) {
+                failedMetrics[metric] = {
+                    value,
+                    budget
+                }
+            }
+        })
+
+        assert.equal(
+            Object.keys(failedMetrics).length, 0,
+            `Following metrics are out of budget ${JSON.stringify(failedMetrics, null, 4)}`
+        )
     })
 
     /**
